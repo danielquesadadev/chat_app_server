b74c4081f3284c7fbdc3bcf13729714323a52ab2
Daniel <danielcrevi@gmail.com>
2023-09-27 15:19:11 +0200

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,120 @@
+const { response } = require("express");
+const  bcrypt  = require('bcryptjs');
+
+const User = require('../models/user');
+const { generateJWT } = require("../helpers/jwt");
+
+const createUser = async ( req, res = response ) => {
+    
+    const {email, password} = req.body;
+    
+    try {
+        
+        const emailExists = await User.findOne({ email });
+        if (emailExists) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ese email ya está registrado.'
+            });
+        }
+        
+        const user = new User( req.body );
+
+        // Encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync( password, salt );
+
+        await user.save();
+
+        // JWT
+        const token = await generateJWT(user._id) ;
+
+        res.json({
+            ok: true,
+            body: user,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador.'
+        });
+    }
+    
+}
+
+const login = async ( req, res = response ) => {
+    
+    const {email, password} = req.body;
+    
+    try {
+        
+        const usuarioDB = await User.findOne({ email });
+    
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Email no encontrado'
+            });
+        }
+
+        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+
+        if(!validPassword){
+            return res.status(400).json({
+                ok: false,
+                msg: 'La contraseña no es válida'
+            });
+        }
+
+        const token = await generateJWT(usuarioDB.uid) ;
+
+
+        res.json({
+            ok: true,
+            body: usuarioDB,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador.'
+        });
+    }
+    
+}
+
+const renewToken = async ( req, res = response) => {
+
+    try {
+        const uid = req.uid;
+
+        const token = await generateJWT(uid);
+            
+        const user = await User.findById(uid);
+
+        res.json({
+            ok: true,
+            user,
+            token
+        });    
+
+    } catch (error) {
+        console.log('Error fetching user:', error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error al obtener el usuario.'
+        });
+    }
+    
+};
+
+module.exports = {
+    createUser,
+    login,
+    renewToken
+}
---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+/* path: api/login */
+const { Router } =  require('express');
+const { createUser, login, renewToken } = require('../controllers/auth');
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validar-campos');
+const { validateJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+router.post('/new' , [
+    check('nombre', 'El nombre es obligatorio.').not().isEmpty(),
+    check('email', 'El email es obligatorio.').isEmail(),
+    check('password', 'La contraseña es obligatoria.').not().isEmpty(),
+    validateFields
+]  ,createUser);
+
+router.post('/' , [
+    check('email', 'El email es obligatorio.').isEmail(),
+    check('password', 'La contraseña es obligatoria.').not().isEmpty(),
+    validateFields
+]  , login);
+
+router.get('/renew', validateJWT, renewToken);
+
+module.exports =  router;
